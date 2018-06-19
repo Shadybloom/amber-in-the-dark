90756b84eba3f2631e13b51bcc311b701e8f9ab0
Shadybloom Applepie <celestia@safe-mail.net>
2018-06-19 15:19:39 +0300

    Экономическая модель.

---
+++ scripts/army-structure-analyser.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------
+# Модули:
+
+# Парсер аргументов командной строки:
+import argparse
+# Переменные операционной системы:
+import os
+# Регулярные выражения:
+import re
+
+# Автоматизацией не пренебрегай:
+# army-structure-analyser.py -sm вооружённые силы | grep Оператор | xclip -i ; xclip -o
+#Оператор гусеничного бульдозера
+#Оператор гусеничного самоходного парома
+#Оператор гусеничного экскаватора
+#Оператор колёсного бульдозера
+#Оператор колёсного трелёвщика
+#Оператор колёсного экскаватора
+# xclip -o | sed "s@\(.*\)@metadict_detail\['&'\] = {\n        'Рядовой':1,\n        'Пистолет-пулемёт':1,\n        '_-Работа & (часов/смену)':8,\n        }\n@"
+
+#-------------------------------------------------------------------------
+# Опции:
+
+# Каталог словарей:
+METADICT_DIR = 'dict'
+# Объект для исследования по умолчанию:
+SQUAD = ''
+# Число однотипных объектов для исследования:
+NUMBER = 1
+# Глубина исследования:
+DEPTH = 99
+
+#-------------------------------------------------------------------------
+# Аргументы командной строки:
+
+def create_parser():
+    """Список доступных параметров скрипта."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('squad',
+                        nargs='*',
+                        help='Название отряда (в "кавычках")'
+                        )
+    parser.add_argument('-D', '--directory',
+                        action='store', dest='dir', type=str,
+                        help='Каталог словарей (путь)'
+                        )
+    parser.add_argument('-d', '--depth',
+                        action='store', dest='depth', type=int,
+                        help='Глубина исследования (0-999)'
+                        )
+    parser.add_argument('-n', '--number',
+                        action='store', dest='number', type=float,
+                        help='Доля отряда, либо число отрядов (0.001-999)'
+                        )
+    parser.add_argument('-e', '--except',
+                        action='store', dest='exc', type=str, nargs='*',
+                        help='Искать в составе отряда (строка в "кавычках")'
+                        )
+    parser.add_argument('-s', '--short',
+                        action='store_true', default='False',
+                        help='Краткий вывод, обобщение, расчёты'
+                        )
+    parser.add_argument('-m', '--model',
+                        action='store_true', default='False',
+                        help='Моделирование, затраты времени'
+                        )
+    return parser
+
+#-------------------------------------------------------------------------
+# Функции:
+
+def metadict_path (metadict_dir):
+    """Возвращает абсолютный путь к каталогу словарей."""
+    # Получаем абсолютный путь к каталогу скрипта:
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    # Добавляем к пути каталог словарей:
+    metadict_path = script_path + '/' + metadict_dir
+    return metadict_path
+
+def find_files (directory):
+    """Возвращает список путей ко всем файлам каталога, включая подкаталоги."""
+    path_f = []
+    for d, dirs, files in os.walk(directory):
+        for f in files:
+                # Формирование адреса:
+                path = os.path.join(d,f)
+                # Добавление адреса в список:
+                path_f.append(path)
+    return path_f
+
+def key_search (search_string, dict):
+    """Поиск нужного объекта в словаре, выбор по списку совпадений."""
+    # Создаётся регистронезависимая поисковая строка:
+    p = re.compile(search_string, re.I)
+    # Создаём словарь совпадений:
+    dict_found = {}
+    counter = 0
+    # Поиск в словаре:
+    for line in sorted(dict.keys()):
+        if p.findall(line):
+            dict_found[counter] = line
+            search_string = line
+            counter = counter + 1
+    # Если искомое не найдено, тогда выход:
+    if not dict_found:
+        print('---Совпадений не найдено')
+        exit(0)
+    # Если найден один варинт, тогда его и выбираем:
+    elif len(dict_found) == 1:
+        squad = dict_found[0]
+    # Если несколько совпадений, тогда выбор по номеру:
+    else:
+        for key in dict_found:
+            print(key,dict_found[key])
+        string_number = input('---Найдено несколько совпадений (введите номер): ')
+        search_string = dict_found[int(string_number)]
+        squad = search_string
+        print('-----------------------------------------------------')
+    return squad
+
+def bfd(vertex, number, dict_crew, metadict_army):
+    """Обработка иерархической базы данных, функция обхода в ширину."""
+    #print ('    vertex:', vertex, number)
+    # Проверка, является ли объект составным:
+    if vertex in metadict_army:
+        # Определяется состав объекта:
+        for key,value in sorted(metadict_army[vertex].items()):
+            #print ('        key:', key, value)
+            if vertex in dict_crew:
+                value = value * dict_crew[vertex]
+            else:
+                value = value * number
+            # Если объект уже есть в рабочем словаре, прибавить:
+            if key in dict_crew:
+                dict_crew[key] = dict_crew[key] + value
+            # Иначе создать новый объект:
+            else:
+                dict_crew[key] = value
+    # Если объект цельный:
+    else:
+        # Проверка, есть ли такие объекты в рабочем словаре:
+        if vertex in dict_crew:
+            # Если да, суммировать:
+            dict_crew[vertex] = dict_crew[vertex] + number
+        else:
+            dict_crew[vertex] = number
+
+#-------------------------------------------------------------------------
+# Обработка аргументов командной строки:
+
+# Создаётся список аргументов скрипта:
+parser = create_parser()
+namespace = parser.parse_args()
+
+# Проверка, указана ли ссылка на каталог словарей (ключ -D --directory):
+if namespace.dir:
+    dir_path = os.path.expanduser(namespace.dir)
+    dicts_list = find_files(dir_path)
+else:
+    dicts_list = find_files(metadict_path(METADICT_DIR))
+
+# Загрузка словарей в базу данных:
+metadict_army = {}
+metadict_detail = {}
+metadict_model = {}
+for file_path in dicts_list:
+    with open(file_path) as f:
+        code = compile(f.read(), file_path, 'exec')
+        exec(code, globals(), locals())
+
+# Проверка, выбран ли словарь деталей (ключ -s --short):
+if namespace.short is True:
+    # Объединяем словари
+    metadict_army.update(metadict_detail)
+
+# Проверка, выбран ли словарь модели (ключ -m --model):
+if namespace.model is True:
+    # Объединяем словари
+    metadict_army.update(metadict_model)
+
+# Проверка, введено ли название отряда:
+if namespace.squad:
+    squad = ' '.join(namespace.squad)
+else:
+    # Если нет, берём стандартный:
+    squad = SQUAD
+
+# Проверка, указано ли число расчётных объектов:
+if namespace.number is not None:
+    obj_number = namespace.number
+else:
+    # Если нет, берём из опций:
+    obj_number = NUMBER
+
+# Если название неточное, срабатывает встроенный поисковик:
+if squad not in metadict_army.keys():
+    squad = key_search(squad, metadict_army)
+    print('Выбрано:', squad, round(obj_number))
+
+# Исключаем (выделяем) указанный объект из словаря:
+if namespace.exc:
+    squad_except = ' '.join(namespace.exc)
+    # Если название неточное, срабатывает встроенный поисковик:
+    if squad_except not in metadict_army.keys():
+        print('-----------------------------------------------------')
+        print('---Число каких объектов вычисляем?')
+        squad_except = key_search(squad_except, metadict_army)
+else:
+    squad_except = None
+
+# Проверка, указана ли глубина исследования:
+if namespace.depth != None:
+    depth = namespace.depth
+else:
+    # Если нет, мспользовать стандартную:
+    depth = DEPTH
+
+#-------------------------------------------------------------------------
+# Исполняемый код:
+
+# Рабочий словарь:
+dict_crew = {}
+
+# Многоуровневый обход в ширину:
+if depth == 0:
+    for key,value in sorted(metadict_army[squad].items()):
+        key = key * obj_number
+        print(key,round(value))
+else:
+    cycles = depth - 1
+    # Формируется временный словарь
+    for key,value in sorted(metadict_army[squad].items()):
+        value = value * obj_number
+        # Первый уровень исследования
+        #print ('-----------------')
+        #print ('cycle:',cycles)
+        bfd(key, value, dict_crew, metadict_army)
+        #print ('    dict:', dict_crew)
+    # Цикл исследования временного словаря
+    while cycles > 0:
+        #print ('-----------------')
+        #print ('cycle:',depth)
+        for key,value in sorted(dict_crew.items()):
+            if key in metadict_army and squad_except != key:
+                # Функция извлекает состав объекта:
+                bfd(key, value, dict_crew, metadict_army)
+                # Удаляется обработанный объект из словаря:
+                dict_crew.pop(key)
+                #print ('    dict:', dict_crew)
+        cycles -= 1
+        depth -= 1
+
+# Вывод данных:
+if namespace.exc:
+    value = dict_crew.pop(squad_except, 0)
+    #print(squad_except, round(value))
+    print ('{0:60} | {1:0}'.format(squad_except, round(value)))
+else:
+    for key,value in sorted(dict_crew.items()):
+        print ('{0:60} | {1:0}'.format(key, round(value)))
+        #print(key, round(value))
+        #print(key)
---
+++ scripts/dict/amber_detail_forests.py
@@ -0,0 +1,335 @@
+#----
+# Леса Эквестрии:
+    # http://botsad.ru/menu/mir-rastenii/rastitelnost-mira/lesa-severnoj-ameriki/
+    # http://www.slate.com/blogs/the_vault/2015/01/09/history_of_american_forests_tree_maps_made_for_1884_census.html
+    # https://www.thoughtco.com/current-us-forest-type-density-maps-1343455
+
+metadict_detail['Лиственные леса (гектар)'] = {
+        'Лиственные леса северного района (гектар)':0.2,
+        'Широколиственные леса центрального района (гектар)':0.3,
+        'Берёзовые леса центрального района (гектар)':0.15,
+        'Дубовые леса центрального района (гектар)':0.3,
+        'Ивняки центрального района (гектар)':0.05,
+        }
+
+metadict_detail['Хвойные леса (гектар)'] = {
+        'Хвойные леса северо-западного района (гектар)':0.2,
+        'Хвойные леса северо-восточного района (гектар)':0.3,
+        'Сосновые леса южного субтропического района (гектар)':0.5,
+        }
+
+#----
+# Рощи:
+
+metadict_detail['Хвойные леса северо-западного района (гектар)'] = {
+        # Состав указан очень приблизительно.
+        'Сосна веймутовая (Pinus strobus) (гектар)':0.7,
+        'Сосна жёсткая (Pinus rigida) (гектар)':0.3,
+        }
+
+metadict_detail['Хвойные леса северо-восточного района (гектар)'] = {
+        'Пихта бальзамическая (Abies balsamea) (гектар)':0.7,
+        'Туя западная (Thuja occidentalis) (гектар)':0.3,
+        }
+
+metadict_detail['Сосновые леса южного субтропического района (гектар)'] = {
+        'Сосна ладанная (Pinus taeda) (гектар)':0.6,
+        'Сосна длиннохвостая (Pinus palustris) (гектар)':0.25,
+        'Сосна озёрная (Pinus serotina) (гектар)':0.11,
+        'Сосна ежовая (Pinus echinata) (гектар)':0.04,
+        }
+
+metadict_detail['Лиственные леса северного района (гектар)'] = {
+        'Дуб горный (Quércus montana) (гектар)':0.2,
+        'Дуб северный (Quércus borealis) (гектар)':0.15,
+        'Дуб бархатистый (Quércus velutina) (гектар)':0.11,
+        'Клён серебристый (Ácer sacchárinum) (гектар)':0.21,
+        'Бук крупнолистный (Fagus grandifolia) (гектар)':0.2,
+        'Каштан зубчатый (Castaner dentata) (гектар)':0.09,
+        'Кария овальная (Carya ovalis) (гектар)':0.04,
+        }
+
+metadict_detail['Берёзовые леса центрального района (гектар)'] = {
+        'Берёза жёлтая (Betula lutea) (гектар)':0.6,
+        'Черёмуха поздняя (Padus serotina) (гектар)':0.2,
+        'Ильм эквианский (Ulmus equesriana) (гектар)':0.15,
+        'Хмелеграб обыкновенный (Ostrya carpinifolia) (гектар)':0.05,
+        }
+
+metadict_detail['Дубовые леса центрального района (гектар)'] = {
+        'Дуб белый (Quércus álba) (гектар)':0.6,
+        'Дуб кашатоновый (Quércus prinus) (гектар)':0.18,
+        'Дуб серповидный (Quércus falcata) (гектар)':0.12,
+        'Дуб красный (Quércus rubra) (гектар)':0.07,
+        'Дуб чёрный (Quércus nigra) (гектар)':0.03,
+        }
+
+metadict_detail['Широколиственные леса центрального района (гектар)'] = {
+        'Клён серебристый (Ácer sacchárinum) (гектар)':0.5,
+        'Бук крупнолистный (Fagus grandifolia) (гектар)':0.2,
+        'Белая акация (Robonia pseudoacacia) (гектар)':0.11,
+        'Клён ясенелистый (Ácer negundo) (гектар)':0.08,
+        'Кария гладкая (Carya glabra) (гектар)':0.07,
+        'Кария белая (Carya alba) (гектар)':0.04,
+        }
+
+metadict_detail['Ивняки центрального района (гектар)'] = {
+        'Ива чёрная (Salix nigra) (гектар)':1,
+        }
+
+#----
+# Растения:
+
+metadict_detail['Белая акация (Robonia pseudoacacia) (гектар)'] = {
+        # Исправить.
+            # Вообще-то густота растений на гектар очень разная.
+            # Но пока пусть будет средней.
+            # Кстати, можно посчитать медоносы.
+            # Можно разделить растения на взрослые и молодые.
+        '|-Белая акация (Robonia pseudoacacia)':250,
+        }
+
+metadict_detail['Берёза жёлтая (Betula lutea) (гектар)'] = {
+        '|-Берёза жёлтая (Betula lutea)':250,
+        }
+
+metadict_detail['Бук крупнолистный (Fagus grandifolia) (гектар)'] = {
+        '|-Бук крупнолистный (Fagus grandifolia)':250,
+        }
+
+metadict_detail['Дуб бархатистый (Quércus velutina) (гектар)'] = {
+        '|-Дуб бархатистый (Quércus velutina)':250,
+        }
+
+metadict_detail['Дуб белый (Quércus álba) (гектар)'] = {
+        '|-Дуб белый (Quércus álba)':250,
+        }
+
+metadict_detail['Дуб горный (Quércus montana) (гектар)'] = {
+        '|-Дуб горный (Quércus montana)':250,
+        }
+
+metadict_detail['Дуб кашатоновый (Quércus prinus) (гектар)'] = {
+        '|-Дуб кашатоновый (Quércus prinus)':250,
+        }
+
+metadict_detail['Дуб красный (Quércus rubra) (гектар)'] = {
+        '|-Дуб красный (Quércus rubra)':250,
+        }
+
+metadict_detail['Дуб северный (Quércus borealis) (гектар)'] = {
+        '|-Дуб северный (Quércus borealis)':250,
+        }
+
+metadict_detail['Дуб серповидный (Quércus falcata) (гектар)'] = {
+        '|-Дуб серповидный (Quércus falcata)':250,
+        }
+
+metadict_detail['Дуб чёрный (Quércus nigra) (гектар)'] = {
+        '|-Дуб чёрный (Quércus nigra)':250,
+        }
+
+metadict_detail['Ива чёрная (Salix nigra) (гектар)'] = {
+        '|-Ива чёрная (Salix nigra)':250,
+        }
+
+metadict_detail['Ильм эквианский (Ulmus equesriana) (гектар)'] = {
+        '|-Ильм эквианский (Ulmus equesriana)':250,
+        }
+
+metadict_detail['Кария белая (Carya alba) (гектар)'] = {
+        '|-Кария белая (Carya alba)':250,
+        }
+
+metadict_detail['Кария гладкая (Carya glabra) (гектар)'] = {
+        '|-Кария гладкая (Carya glabra)':250,
+        }
+
+metadict_detail['Каштан зубчатый (Castaner dentata) (гектар)'] = {
+        '|-Каштан зубчатый (Castaner dentata)':250,
+        }
+
+metadict_detail['Кария овальная (Carya ovalis) (гектар)'] = {
+        '|-Кария овальная (Carya ovalis)':250,
+        }
+
+metadict_detail['Клён серебристый (Ácer sacchárinum) (гектар)'] = {
+        '|-Клён серебристый (Ácer sacchárinum)':250,
+        }
+
+metadict_detail['Клён ясенелистый (Ácer negundo) (гектар)'] = {
+        '|-Клён ясенелистый (Ácer negundo)':250,
+        }
+
+metadict_detail['Пихта бальзамическая (Abies balsamea) (гектар)'] = {
+        '|-Пихта бальзамическая (Abies balsamea)':250,
+        }
+
+metadict_detail['Сосна веймутовая (Pinus strobus) (гектар)'] = {
+        '|-Сосна веймутовая (Pinus strobus)':250,
+        }
+
+metadict_detail['Сосна длиннохвостая (Pinus palustris) (гектар)'] = {
+        '|-Сосна длиннохвостая (Pinus palustris)':250,
+        }
+
+metadict_detail['Сосна ежовая (Pinus echinata) (гектар)'] = {
+        '|-Сосна ежовая (Pinus echinata)':250,
+        }
+
+metadict_detail['Сосна жёсткая (Pinus rigida) (гектар)'] = {
+        '|-Сосна жёсткая (Pinus rigida)':250,
+        }
+
+metadict_detail['Сосна ладанная (Pinus taeda) (гектар)'] = {
+        '|-Сосна ладанная (Pinus taeda)':250,
+        }
+
+metadict_detail['Сосна озёрная (Pinus serotina) (гектар)'] = {
+        '|-Сосна озёрная (Pinus serotina)':250,
+        }
+
+metadict_detail['Туя западная (Thuja occidentalis) (гектар)'] = {
+        '|-Туя западная (Thuja occidentalis)':250,
+        }
+
+metadict_detail['Хмелеграб обыкновенный (Ostrya carpinifolia) (гектар)'] = {
+        '|-Хмелеграб обыкновенный (Ostrya carpinifolia)':250,
+        }
+
+metadict_detail['Черёмуха поздняя (Padus serotina) (гектар)'] = {
+        '|-Черёмуха поздняя (Padus serotina)':250,
+        }
+
+#----
+# Растения (упрощение):
+
+metadict_detail['|-Белая акация (Robonia pseudoacacia)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Берёза жёлтая (Betula lutea)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Бук крупнолистный (Fagus grandifolia)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Виноград культурный (Vítis vinífera)'] = {
+        # Молодых деревьев примерно 10% от общего числа.
+        '|--Плодовые деревья':1 / 1.1,
+        }
+
+metadict_detail['|-Дуб бархатистый (Quércus velutina)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб белый (Quércus álba)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб горный (Quércus montana)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб кашатоновый (Quércus prinus)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб красный (Quércus rubra)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб северный (Quércus borealis)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб серповидный (Quércus falcata)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Дуб чёрный (Quércus nigra)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Ива чёрная (Salix nigra)'] = {
+        '|--Прутовые деревья':1 / 5,
+        }
+
+metadict_detail['|-Ильм эквианский (Ulmus equesriana)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Кария белая (Carya alba)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Кария гладкая (Carya glabra)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Кария овальная (Carya ovalis)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Каштан зубчатый (Castaner dentata)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Клён серебристый (Ácer sacchárinum)'] = {
+        '|--Дровяные деревья':1 / 10,
+        #'|--Клён сахарный':1 / 10,
+        }
+
+metadict_detail['|-Клён ясенелистый (Ácer negundo)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Пихта бальзамическая (Abies balsamea)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Сосна веймутовая (Pinus strobus)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Сосна длиннохвостая (Pinus palustris)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Сосна ежовая (Pinus echinata)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Сосна жёсткая (Pinus rigida)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Сосна ладанная (Pinus taeda)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Сосна озёрная (Pinus serotina)'] = {
+        '|--Строевые деревья':1 / 50,
+        }
+
+metadict_detail['|-Туя западная (Thuja occidentalis)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Хмелеграб обыкновенный (Ostrya carpinifolia)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Черёмуха поздняя (Padus serotina)'] = {
+        '|--Дровяные деревья':1 / 10,
+        }
+
+metadict_detail['|-Яблоня карликовая (Malus pumila)'] = {
+        '|--Плодовые деревья':1 / 1.1,
+        }
+
+metadict_detail['|-Кофе робуста (Coffea robusta)'] = {
+        '|--Плодовые деревья':1 / 1.1,
+        }
