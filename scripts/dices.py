fd50f39e68f759c04e18098ebfd08c09d8374876
Shadybloom Applepie <celestia@safe-mail.net>
2022-11-03 03:21:57 +0300

    Модель Янтаря. Костеметалка.

--- scripts/dices.py
+++ scripts/dices.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Скрипт бросает игральные кости и выдаёт результат.
+# Один бросок шестигранной кости:
+# dices 1d6
+# Четыре броска восьмигранной кости:
+# dices 4d8
+# Тысяча бросков тридцатигранной кости:
+# dices 1000d30
+
 import random
+import argparse
+
+#-------------------------------------------------------------------------
+# Опции:
+
+# Стандартная кость:
+DICE = 20
+# Число бросков стандартной кости:
+DICE_THROWS = 1
+
+#-------------------------------------------------------------------------
+# Аргументы командной строки:
+
+def create_parser():
+    """Список доступных параметров скрипта."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dice_string',
+                        action='store', type=str, nargs='*', default='',
+                        help='Кость D&D, например 2d6 (по умолчанию 1d20)'
+                        )
+    return parser
+
+#-------------------------------------------------------------------------
+# Функции:
 
 def dice_range (n1, n2, throws=1):
     """Возвращает рандомное число в диапазоне."""
@@ -14,6 +46,9 @@ def dice_range (n1, n2, throws=1):
     return d_sum
 
 def throw_d2(throws=1):
+    # Функции ниже для внешних вызовов.
+    # Сам скрипт использут только dice_range
+    """Бросаем двухгранную кость (монетку)."""
     d = dice_range(1, 2, throws)
     return d
 
@@ -22,90 +57,56 @@ def throw_d3(throws=1):
     return d
 
 def throw_d4(throws=1):
+    """Бросаем четырёхгранную кость."""
     d = dice_range(1, 4, throws)
     return d
 
 def throw_d6(throws=1):
+    """Бросаем шестигранную кость."""
     d = dice_range(1, 6, throws)
     return d
 
 def throw_d8(throws=1):
+    """Бросаем восьмигранную кость."""
     d = dice_range(1, 8, throws)
     return d
 
 def throw_d10(throws=1):
+    """Бросаем десятигранную кость."""
     d = dice_range(1, 10, throws)
     return d
 
 def throw_d12(throws=1):
+    """Бросаем двенацтигранную кость."""
     d = dice_range(1, 12, throws)
     return d
 
 def throw_d20(throws=1):
+    """Бросаем двадцатигранную кость."""
     d = dice_range(1, 20, throws)
     return d
 
 def throw_d100(throws=1):
+    """Бросаем стогранную кость (проценты)."""
     d = dice_range(1, 100, throws)
     return d
-
-def ability_gen():
-    """Создаём параметры персонажа по правилам D&D 3.5
-    
-    1) Под каждый параметр бросаем 4 кости на d6
-    2) Получается список чисел (например 1,4,6,3)
-    3) Меньшее число из списка отбрсываем (получается 4,6,3)
-    3) Суммируем показатели, получаем число в диапазоне 3-18
-    4) Повторяем шесть раз.
-    """
-    ability_list = []
-    while len(ability_list) < 6:
-        # Четыре раза бросаем кость d6, сохраняем все значения:
-        trows_list = [throw_d6() for x in range(4)]
-        # Удаляем меньшее значение
-        trows_list.remove(min(trows_list))
-        ability = sum(trows_list)
-        ability_list.append(ability)
-    return ability_list
-
-def ability_gen_max(ability_sum=60, ability_min=8, ability_max=13):
-    """Создаём крутого персонажа, слабаков отбраковываем."""
-    ability_list = ability_gen()
-    # Исправить
-        # Почему and не срабатывает?
-    print(ability_list)
-    while max(ability_list) <= ability_max \
-    and sum(ability_list) <= ability_sum \
-    and min(ability_list) >= ability_min:
-        ability_list = ability_gen()
-        print(ability_list)
-    return ability_list
-
-def hit_trow(level=1):
-    """Бросаем кубики хит-поинтов."""
-    # Исправить
-        # Допилить
-    hits = throw_d4(level)
-    return ability_list
-
 
 #-------------------------------------------------------------------------
 # Тело программы:
 
 if __name__ == '__main__':
-    ability_gen_max()
-    print(throw_d4(20))
-    #print(throw_d4(1))
-    #print(throw_d4(1))
-    #print(throw_d4(1))
-    #print(throw_d4(1))
-    #print(throw_d8(1))
-    #print(throw_d8(1))
-    #print(throw_d8(1))
-    #print(throw_d8(1))
-    #print(throw_d8(1))
-    #print(throw_d6(1))
-    #print(throw_d6(1))
-    #print(throw_d6(1))
-    #print(throw_d6(1))
-    #print(throw_d6(1))
+    # Создаётся список аргументов скрипта:
+    parser = create_parser()
+    namespace = parser.parse_args()
+    if namespace.dice_string:
+        # Бросок записывается как 2d6 (2 броска кости 6-гранной)
+        # Вводим строку и дробим её по разделителю 'd' на два элемента.
+        # Сначала число бросков, затем число граней кости.
+        throws_dice_list = (namespace.dice_string[0].split('d'))
+        throws = int(throws_dice_list[0])
+        dice = int(throws_dice_list[1])
+    else:
+        throws = DICE_THROWS
+        dice = DICE
+    output = dice_range(1, dice, throws)
+    print(output)
